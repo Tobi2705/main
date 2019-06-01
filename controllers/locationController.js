407cf61331bbc7d5801cd670a58949e79b3b7788
Toomaime <tobiasrohmann@tobiass-mbp.speedport.ip>
2019-06-01 14:55:38 +0200

    make it betteeeee

---
+++ controllers/locationController.js
@@ -0,0 +1,178 @@
+const mongoose = require('mongoose');
+var posts = require('../models/posts');
+var async = require('async');
+var posts = require('../models/posts');
+var post = require('../models/posts');
+require('../models/posts');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://Tobi:270582@ds223009.mlab.com:23009/sportjobs";
+const keySecret = "sk_test_ePJLtrTR2io0TGQ7W8QMGzz2";
+const stripe = require("stripe")(keySecret);
+
+exports.getLocations = (req, res) => {
+    res.render('locations');
+}
+
+exports.locationsLondon = (req, res) => {
+    async.parallel({
+        data: function (callback) {
+            post.find({
+                $and: [{
+                    $or: [{
+                        location: /London/
+                    }]
+                },
+                {
+                    $or: [{
+                        paid: false
+                    }]
+                }
+                ]
+            })
+                .sort({
+                    "date": -1
+                })
+                .limit(15)
+                .exec(callback)
+
+        },
+        paid: function (callback) {
+            post.find({
+                $and: [{
+                    $or: [{
+                        location: /London/
+                    }]
+                },
+                {
+                    $or: [{
+                        paid: true
+                    }]
+                }
+                ]
+            })
+                .sort({
+                    "date": -1
+                })
+                .exec(callback)
+        },
+    }, function (err, listings) {
+        if (err) {
+            return next(err);
+        } // Error in API usage.
+        // Successful, so render.
+        res.render('locationJobs', {
+            data: listings.data,
+            paid: listings.paid,
+            location: "London"
+        });
+    });
+};
+
+exports.locationsAmsterdam = (req, res) => {
+    async.parallel({
+        data: function (callback) {
+            post.find({
+                $and: [{
+                    $or: [{
+                        location: /Amsterdam/
+                    }]
+                },
+                {
+                    $or: [{
+                        paid: false
+                    }]
+                }
+                ]
+            })
+                .sort({
+                    "date": -1
+                })
+                .limit(15)
+                .exec(callback)
+
+        },
+        paid: function (callback) {
+            post.find({
+                $and: [{
+                    $or: [{
+                        location: /Amsterdam/
+                    }]
+                },
+                {
+                    $or: [{
+                        paid: true
+                    }]
+                }
+                ]
+            })
+                .sort({
+                    "date": -1
+                })
+                .exec(callback)
+        },
+    }, function (err, listings) {
+        if (err) {
+            return next(err);
+        } // Error in API usage.
+        // Successful, so render.
+        res.render('locationJobs', {
+            data: listings.data,
+            paid: listings.paid,
+            location: "Amsterdam"
+        });
+    });
+};
+exports.locationsBerlin = (req, res) => {
+    async.parallel({
+        data: function (callback) {
+            post.find({
+                $and: [{
+                    $or: [{
+                        location: /Berlin/
+                    }]
+                },
+                {
+                    $or: [{
+                        paid: false
+                    }]
+                }
+                ]
+            })
+                .sort({
+                    "date": -1
+                })
+                .limit(15)
+                .exec(callback)
+
+        },
+        paid: function (callback) {
+            post.find({
+                $and: [{
+                    $or: [{
+                        location: /Berlin/
+                    }]
+                },
+                {
+                    $or: [{
+                        paid: true
+                    }]
+                }
+                ]
+            })
+                .sort({
+                    "date": -1
+                })
+                .exec(callback)
+        },
+    }, function (err, listings) {
+        if (err) {
+            return next(err);
+        } // Error in API usage.
+        // Successful, so render.
+        res.render('locationJobs', {
+            data: listings.data,
+            paid: listings.paid,
+            location: "Berlin"
+        });
+    });
+};
